50a4c5d79d7b4658bd5bfd17433758ab75605231
David Amebley <davameb@gmail.com>
2024-04-26 14:07:49 +0300

    Add HttpClientModule and RouterModule in Main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { appConfig } from './app/app.config';
+import { RouterModule } from '@angular/router';
+import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(HttpClientModule, RouterModule.forRoot(routes, { enableTracing: true })),
+    ...appConfig.providers  // Spread the providers from appConfig
+  ]
+}).catch((err) => console.error(err));
