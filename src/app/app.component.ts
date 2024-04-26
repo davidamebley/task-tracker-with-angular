3318c7dd8c834f111e58e5160a7b85681e8b3188
David Amebley <davameb@gmail.com>
2024-04-26 14:11:33 +0300

    Import major components in App.Components.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { TasksComponent } from "./components/tasks/tasks.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, HeaderComponent, TasksComponent, FooterComponent]
 })
 export class AppComponent {
-  title = 'task-tracker';
+
 }
