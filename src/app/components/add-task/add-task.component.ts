f315a68a565266de23f16a2766ae41ef4c3b6c42
David Amebley <davameb@gmail.com>
2024-04-26 14:05:03 +0300

    Add Add Task component

---
+++ src/app/components/add-task/add-task.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Subscription } from 'rxjs';
+
+import { Task } from '../../Task';
+import { UiService } from '../../services/ui.service';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-add-task',
+  standalone: true,
+  imports: [FormsModule, NgIf],
+  templateUrl: './add-task.component.html',
+  styleUrl: './add-task.component.css'
+})
+export class AddTaskComponent implements OnInit {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+
+  text: string = '';
+  day: string = '';
+  reminder: boolean = false;
+  showAddTask: boolean = false;
+  subscription: Subscription = new Subscription;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    if (!this.text) {
+      alert('Please enter a task');
+      return;
+    }
+
+    const newTask = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder
+    }
+
+    this.onAddTask.emit(newTask);
+
+    // Clear form
+    this.text = '';
+    this.day = '';
+    this.reminder = false;
+  }
+
+}
